5c5a1fa9746076f60c86d226ad94931c056e76f8
kdolayao <65010703+kdolayao@users.noreply.github.com>
2020-10-04 13:42:52 +0800

    Remove firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,6 @@
 import firebase from "firebase";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyD_UA3NJk-9kXvftfrDrc8wSqDZavDxmS4",
-  authDomain: "whatsapp-clone-6bfeb.firebaseapp.com",
-  databaseURL: "https://whatsapp-clone-6bfeb.firebaseio.com",
-  projectId: "whatsapp-clone-6bfeb",
-  storageBucket: "whatsapp-clone-6bfeb.appspot.com",
-  messagingSenderId: "82848931618",
-  appId: "1:82848931618:web:13320aa04e8e211ace8f7b",
-  measurementId: "G-SLW90PG39V",
-};
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 
